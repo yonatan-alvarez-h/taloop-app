a82e60a64875deb1bd42bdee18b49fc278bbdd47
Yonatan Alvarez H <yonatan.alvarez.h@gmail.com>
2025-07-01 22:05:13 -0500

    feat: update App component to include BurgerMenu and SearchBar with search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import Card from "./components/Card";
+import BurgerMenu from "./components/menu/BurgerMenu";
+import SearchBar from "./components/search/SearchBar";
 
 function App() {
-  return <Card />;
+  const handleSearch = (query: string) => {
+    // Aquí puedes manejar la búsqueda de contenido
+    alert(`Buscando: ${query}`);
+  };
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light px-3 d-flex align-items-center justify-content-between">
+        <div className="d-flex align-items-center">
+          <BurgerMenu />
+          <span className="navbar-brand mb-0 h1 ms-2">taloop</span>
+        </div>
+        <div style={{ width: "300px" }}>
+          <SearchBar onSearch={handleSearch} />
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
