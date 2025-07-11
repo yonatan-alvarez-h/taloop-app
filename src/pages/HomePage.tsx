62b1caca9d9ca037fe73c1b7d8bf9e1765f14850
Yonatan Alvarez H <yonatan.alvarez.h@gmail.com>
2025-07-10 21:45:22 -0500

    feat: enhance SearchBar and HomePage styles for improved layout and consistency

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -14,8 +14,7 @@ interface HomePageProps {
 const HomePage: React.FC<HomePageProps> = ({ datasets, search, onSearch }) => (
   <div>
     <nav className="homepage-navbar">
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <BurgerMenu />
+      <div className="homepage-navbar-brand">
         <span className="navbar-brand ms-2">taloop</span>
       </div>
       <div className="homepage-searchbar">
