987b245c30f97cc2c40e0f8c55e0a62877400b59
Yonatan Alvarez H <yonatan.alvarez.h@gmail.com>
2025-07-17 23:46:03 -0500

    Enhance NavBar branding with pulsating effect; adjust styles for improved visual appeal and responsiveness across devices.

--- src/components/menu/nav/NavBar.tsx
+++ src/components/menu/nav/NavBar.tsx
@@ -4,7 +4,10 @@ import "./NavBar.css";
 const NavBar: React.FC = () => {
   return (
     <div className="navbar-brand">
-      <span className="navbar-brand ms-2">taloop</span>
+      <span className="navbar-brand ms-2">
+        ta
+        <span className="navbar-brand-pulse navbar-brand-pulse-2">loop</span>
+      </span>
     </div>
   );
 };
