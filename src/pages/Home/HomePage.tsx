792d5fba986b790c1f8afb880af311a49eebe866
Yonatan Alvarez H <yonatan.alvarez.h@gmail.com>
2025-07-17 22:24:06 -0500

    Refactor global styles and HomePage component to improve scroll management; implement conditional scroll handling for better layout consistency and responsiveness.

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../../components/Menu/Nav/NavBar";
 import SearchBar from "../../components/Menu/Search/SearchBar";
 import DatasetList from "../../components/Dataset/List/DatasetList";
@@ -12,20 +12,32 @@ interface HomePageProps {
   onSearch: (query: string) => void;
 }
 
-const HomePage: React.FC<HomePageProps> = ({ datasets, search, onSearch }) => (
-  <div className="homepage-wrapper">
-    <nav className="navbar">
-      <NavBar />
-      <div className="homepage-searchbar">
-        <SearchBar onSearch={onSearch} />
-      </div>
-    </nav>
-    <div className="homepage-container container">
-      <div className="homepage-content">
-        <DatasetList datasets={datasets} search={search} outerPagination />
+const HomePage: React.FC<HomePageProps> = ({ datasets, search, onSearch }) => {
+  // Aplicar clase para eliminar scroll global solo en HomePage
+  useEffect(() => {
+    document.body.classList.add("no-global-scroll");
+
+    // Cleanup: remover la clase cuando se desmonte el componente
+    return () => {
+      document.body.classList.remove("no-global-scroll");
+    };
+  }, []);
+
+  return (
+    <div className="homepage-wrapper">
+      <nav className="navbar">
+        <NavBar />
+        <div className="homepage-searchbar">
+          <SearchBar onSearch={onSearch} />
+        </div>
+      </nav>
+      <div className="homepage-container container">
+        <div className="homepage-content">
+          <DatasetList datasets={datasets} search={search} outerPagination />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default HomePage;
